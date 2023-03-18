82a92952d20bce2315c383a35fb9b87cfb21a3bc
RuanFigueiredo <ruanthiago67@gmail.com>
2023-03-17 22:59:56 -0300

    componente filho

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
+//import Primeiro from './components/Primeiro'
+//import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
     <div>
-        <Primeiro/>,
+        <ComFilhos>
+            <ul>
+                <li>João Benedito</li>
+                <li>Maria das Graças</li>
+                <li>Jébio</li>
+                <li>Cássio</li>
+            </ul>
+        </ComFilhos>
+        {/* <Primeiro/>,
         <ComParametro Titulo="Esse é o titulo" Subtitulo ="Esse é o subtitulo"/>,
-        <ComParametro Titulo="Opa" Subtitulo ="Epa kk"/>,
+        <ComParametro Titulo="Opa" Subtitulo ="Epa"/>, */}
     </div>,
     
     document.getElementById('root')
